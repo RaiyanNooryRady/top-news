642b23ebbb02ed2c1f4b0efc4fd36454ba1090ae
raiyannooryrady <raiyannooryrady@gmail.com>
2024-02-17 19:24:05 +0600

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Post from './Post';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">TopNews</a>
+        <nav>
+          <a href="">Login</a>
+          <a href=""> Register </a>
+        </nav>
       </header>
-    </div>
+      <Post/>
+
+    </main>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <image>
+        <img src="https://cdn.pixabay.com/photo/2018/09/12/22/43/bangladesh-3673378_1280.jpg" alt="" />
+        </image>
+        
+        <div className="texts">
+          <h2>It is time for a new Africa beyond borders and boundaries</h2>
+          <p className="info">
+            <a className="author">Raiyan Noory</a>
+            <time>17-02-2024 7:08</time>
+          </p>
+          <p className="sumamry">African leaders will be convening in Ethiopia’s capital, Addis Ababa, for the 37th summit of the African Union on Saturday. Donkey thefts like this have become increasingly common across many parts of Africa - and in other parts of the world that have large populations of these working animals. </p>
+        </div>
+
+      </div>
+    );
+}
