ee03012dc772152941a7e98d6cc4b15aeff90db5
raiyannooryrady <raiyannooryrady@gmail.com>
2024-02-18 11:33:45 +0600

    post functionality commented

--- api/index.js
+++ api/index.js
@@ -2,16 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app= express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+ 
+const fs = require('fs');
+
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 mongoose.connect('mongodb+srv://raiyannooryrady:KisherPass123@cluster0.rastlvz.mongodb.net/?retryWrites=true&w=majority');
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
@@ -57,6 +67,75 @@ app.post('/register', async (req,res) => {
   app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok');
   });
+  app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
   
+    // const {token} = req.cookies;
+    // jwt.verify(token, secret, {}, async (err,info) => {
+    //   if (err) throw err;
+    //   const {title,summary,content} = req.body;
+    //   const postDoc = await Post.create({
+    //     title,
+    //     summary,
+    //     content,
+    //     cover:newPath,
+    //     author:info.id,
+    //   });
+    //   res.json(postDoc);
+      //res.json({ext});
+    });
+  
+//   });
+  
+//   app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+//     let newPath = null;
+//     if (req.file) {
+//       const {originalname,path} = req.file;
+//       const parts = originalname.split('.');
+//       const ext = parts[parts.length - 1];
+//       newPath = path+'.'+ext;
+//       fs.renameSync(path, newPath);
+//     }
+  
+//     const {token} = req.cookies;
+//     jwt.verify(token, secret, {}, async (err,info) => {
+//       if (err) throw err;
+//       const {id,title,summary,content} = req.body;
+//       const postDoc = await Post.findById(id);
+//       const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+//       if (!isAuthor) {
+//         return res.status(400).json('you are not the author');
+//       }
+//       await postDoc.update({
+//         title,
+//         summary,
+//         content,
+//         cover: newPath ? newPath : postDoc.cover,
+//       });
+  
+//       res.json(postDoc);
+//     });
+  
+//   });
+  
+//   app.get('/post', async (req,res) => {
+//     res.json(
+//       await Post.find()
+//         .populate('author', ['username'])
+//         .sort({createdAt: -1})
+//         .limit(20)
+//     );
+//   });
+  
+//   app.get('/post/:id', async (req, res) => {
+//     const {id} = req.params;
+//     const postDoc = await Post.findById(id).populate('author', ['username']);
+//     res.json(postDoc);
+//   })
+   
 app.listen(4000);
 //KisherPass123
