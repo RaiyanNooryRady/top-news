fd50b3d6f7fd98c36f716cb10e0c4d16e55163ec
raiyannooryrady <raiyannooryrady@gmail.com>
2024-02-18 10:21:10 +0600

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,21 @@ import Layout from './Layout';
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import {UserContextProvider} from "./UserContext";
 import CreatePost from "./pages/CreatePost";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/create" element={<CreatePost />} />
-      </Route>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+        </Route>
 
-    </Routes>
+      </Routes>
+    </UserContextProvider>
 
   );
 }
