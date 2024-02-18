8000c403d7dc52fad7341870b8173fee74ceea0d
raiyannooryrady <raiyannooryrady@gmail.com>
2024-02-18 10:29:53 +0600

    login logout ok

--- api/index.js
+++ api/index.js
@@ -28,29 +28,35 @@ app.post('/register', async (req,res) => {
      
   });
 
-//   app.post('/login', async (req,res) => {
-//     const {username,password} = req.body;
-//     const userDoc = await User.findOne({username});
-//     const passOk = bcrypt.compareSync(password, userDoc.password);
-//     if (passOk) {
-//       // logged in
-//       jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
-//         if (err) throw err;
-//         res.cookie('token', token).json({
-//           id:userDoc._id,
-//           username,
-//         });
-//       });
-//     } else {
-//       res.status(400).json('wrong credentials');
-//     }
-//   });
-//   app.get('/profile', (req,res) => {
-//     const {token} = req.cookies;
-//     jwt.verify(token, secret, {}, (err,info) => {
-//       if (err) throw err;
-//       res.json(info);
-//     });
-//   });
+  app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+  
+  app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  });
+  
+  app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
+  });
+  
 app.listen(4000);
 //KisherPass123
