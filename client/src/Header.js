7f246fc57ab0e6ceeafdba121894f19270183364
raiyannooryrady <raiyannooryrady@gmail.com>
2024-02-18 15:58:45 +0600

    except edit button all ok

--- client/src/Header.js
+++ client/src/Header.js
@@ -26,7 +26,7 @@ export default function Header() {
 
   return (
     <header>
-      <Link to="/" className="logo">MyBlog</Link>
+      <Link to="/" className="logo">TopNews</Link>
       <nav>
         {username && (
           <>
